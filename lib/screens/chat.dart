ad0002014fe53c7a7257e020624f487d68edeacb
Ruan Emanuell <ruanvitorcom540@gmail.com>
2022-10-11 11:12:10 -0300

    finished chat screen visual

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import "package:firebase_core/firebase_core.dart";
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
-import "../widgets/chatwidgets.dart";
 
 class ChatScreen extends StatefulWidget{
 
@@ -18,21 +17,68 @@ class _ChatScreen extends State<ChatScreen>{
 
   @override
   Widget build(BuildContext context){
+    var statusBarHeight = MediaQuery.of(context).viewPadding.top;
+    var screenHeight=MediaQuery.of(context).size.height;
+    var screenWidth=MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar:AppBar(
-        backgroundColor:Colors.purple,
-        title:Text("RTChat", style:GoogleFonts.rubik(
-          textStyle:TextStyle(
-            fontSize:30,
-            color:Colors.white
+      appBar:PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: Container(
+          height:120,
+          color: Colors.purple,
+          child:Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin:EdgeInsets.only(top: statusBarHeight, left:20),   
+                  child: Text("RTChat", style:GoogleFonts.rubik(
+                  textStyle:TextStyle(
+                    fontSize:30,
+                    color:Colors.white
+                  )
+                 )),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin:EdgeInsets.only(left: screenWidth/5, top:20),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed:(){
+
+                        },
+                        icon:Icon(Icons.search, size:40, color:Colors.white)
+                      ),
+                      IconButton(
+                        onPressed:(){
+                          FirebaseAuth.instance.signOut();
+                        },
+                        icon:Icon(Icons.exit_to_app, size:40, color:Colors.white)    
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
           )
-        ))
+        )
       ),
-      body: Center(
-        child:Column(
+      body: ListView(
+        children:[
+          Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [ 
+            for(var i=0; i<10; i++)
+            Container(
+              margin:EdgeInsets.all(20),
+              child: Text(user.email!, style:TextStyle(
+                fontSize:20
+              ))
+            )
           ],
         )
+        ]
       ),
     );
   }
